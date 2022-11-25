af8f55d931bdf4ee286e1ec5ff5850ee3bcc07aa
jgarc174.ext <jgarc174.ext@PC-CONTROL3-NEW.ttec.es>
2022-11-25 19:51:09 +0100

    update

--- Practica_Cliente_Periodista/src/Cliente/Main.java
+++ Practica_Cliente_Periodista/src/Cliente/Main.java
@@ -1,10 +1,14 @@
 package Cliente;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		Cliente cliente = new Cliente();
+		System.out.println("Iniciando nuevo cliente");
+		cliente.iniciarCliente();
 	}
 
 }
